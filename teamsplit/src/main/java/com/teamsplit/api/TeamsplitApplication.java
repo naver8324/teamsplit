c0886c814107e8980573fd5e6603d1e9179b71d1
[Cloud2] 정주용 <dusk3236@gmail.com>
2026-02-23 00:33:50 +0900

    B chore: initial project setup (#1)

---
+++ teamsplit/src/main/java/com/teamsplit/api/TeamsplitApplication.java
@@ -0,0 +1,13 @@
+package com.teamsplit.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamsplitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamsplitApplication.class, args);
+	}
+
+}
---
+++ teamsplit/src/test/java/com/teamsplit/api/TeamsplitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamsplit.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamsplitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
